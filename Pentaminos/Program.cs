3848c3ae6a61877d25c1bdcc2e1ada35110fb826
BrunoO <bruno.orsier@gmail.com>
2018-11-23 17:16:21 +0100

    nettoyage using pas nécessaires

--- Pentaminos/Program.cs
+++ Pentaminos/Program.cs
@@ -1,12 +1,10 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 
 
 namespace Pentaminos
 {
-    
+
     public class Program
     {        
         static public string Description
