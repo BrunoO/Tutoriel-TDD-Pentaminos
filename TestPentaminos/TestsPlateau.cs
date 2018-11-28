a00ec4c9e95a5cec5da1f57a66637deb66ba07bb
BrunoO <bruno.orsier@gmail.com>
2018-11-28 15:04:22 +0100

    nettoyage code inutile (TestContext)

--- TestPentaminos/TestsPlateau.cs
+++ TestPentaminos/TestsPlateau.cs
@@ -13,44 +13,10 @@ namespace TestsUnitairesPentaminos
         private Plateau plateau;
         private Pentamino I, X;
 
-        public TestsPlateau()
-        {
-            //
-            // TODO: Add constructor logic here
-            //
-        }
-
-        private TestContext testContextInstance;
-
-        /// <summary>
-        ///Gets or sets the test context which provides
-        ///information about and functionality for the current test run.
-        ///</summary>
-        public TestContext TestContext
-        {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
-        }
+     
 
         #region Additional test attributes
-        //
-        // You can use the following additional attributes as you write your tests:
-        //
-        // Use ClassInitialize to run code before running the first test in the class
-        // [ClassInitialize()]
-        // public static void MyClassInitialize(TestContext testContext) { }
-        //
-        // Use ClassCleanup to run code after all tests in a class have run
-        // [ClassCleanup()]
-        // public static void MyClassCleanup() { }
-        //
-        // Use TestInitialize to run code before running each test 
+
          [TestInitialize()]
          public void MyTestInitialize()
          {
@@ -59,10 +25,6 @@ namespace TestsUnitairesPentaminos
              X = FabriqueDePentaminos.ListeDePentaminos(10)[2];
          }
         
-        // Use TestCleanup to run code after each test has run
-        // [TestCleanup()]
-        // public void MyTestCleanup() { }
-        //
         #endregion
 
 
